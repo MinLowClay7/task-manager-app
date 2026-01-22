cffdbc6ad33657c10a5ba71db596399a16021d6a
MinLowClay7 <andresmauricio6777@gmail.com>
2026-01-21 20:03:44 -0500

    polish app UX, error handling and production readiness

---
+++ README.md
@@ -0,0 +1,23 @@
+# Task Manager Desktop
+
+Aplicación de escritorio desarrollada en Python con PySide6.
+
+## Features
+- CRUD de tareas
+- Persistencia local en JSON
+- Dark / Light mode
+- Arquitectura MVC
+- UI moderna
+
+## Tecnologías
+- Python
+- PySide6 (Qt)
+- MVC
+- Git
+
+## Screenshots
+
+
+## Cómo ejecutar
+pip install -r requirements.txt
+python main.py
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QLabel
 from app.ui.main_window import MainWindow
 from PySide6.QtGui import QIcon
-
+from PySide6.QtWidgets import QMessageBox
 
 
 def main():
@@ -18,6 +18,12 @@ def load_styles(app):
     with open("app/ui/styles.qss", "r", encoding="utf-8") as f:
         app.setStyleSheet(f.read())
 
+def show_error(self, message):
+    QMessageBox.critical(
+        None,
+        "Error",
+        message
+    )
 
 if __name__ == "__main__":
     main()
